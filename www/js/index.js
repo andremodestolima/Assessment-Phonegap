5d0c4d58a7c65ed5072a1c129062f714e0b22dd4
Andre Modesto Lima <modesto_ligeiro@yahoo.com.br>
2016-12-16 21:01:55 -0200

    v0.1

--- www/js/index.js
+++ www/js/index.js
@@ -8,6 +8,7 @@ function pronto() {
         facebookConnectPlugin.api('me', ['public_profile'], function(dados){
             localStorage.setItem('nome', dados.name);
             alert(JSON.stringify(dados));
+            alert(JSON.stringify(dados.name));
             document.getElementById("nomePerfil").innerHtml = dados.name;
         //    document.getElementById("imagemPerfil").src = dados.cover;
             // alert(dados.cover);
