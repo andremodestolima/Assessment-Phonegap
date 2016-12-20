8904c095bdaf7ef37a6b91e0aadb253e62d8fdbc
Andre Modesto Lima <modesto_ligeiro@yahoo.com.br>
2016-12-20 12:33:29 -0200

    ratched

--- www/js/index.js
+++ www/js/index.js
@@ -4,16 +4,14 @@ function pronto() {
     function entrarNoFace(){
         navigator.vibrate(200);
         facebookConnectPlugin.login(['public_profile'], function(sucesso){
-            alert("login OK!!");
             // var facebook_id = sucesso.authResponse.userID;
             facebookConnectPlugin.api('me', ['public_profile'], function(dados){
-                alert(JSON.stringify(dados));
                 localStorage.setItem('facebook_nome', dados.name);
                 localStorage.setItem('facebook_id', dados.id);
                 document.getElementById("nomePerfil").innerHTML = dados.name;
                 document.getElementById("imagemPerfil").src = "https://graph.facebook.com/"+dados.id+"/picture/?type=large";
             })
-        }, function(erro) { alert('Não foi possível conectar ao Facebook: '+ erro); });
+        }, function(erro) { alert('Não foi possível conectar ao Facebook!'); });
     }
 }
 
