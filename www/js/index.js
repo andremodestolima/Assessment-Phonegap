6e215eca43c0fa4aa30a798d9906a42d5b9891d2
Andre Modesto Lima <modesto_ligeiro@yahoo.com.br>
2016-12-20 10:43:11 -0200

    ratched

--- www/js/index.js
+++ www/js/index.js
@@ -2,21 +2,20 @@ function pronto() {
     document.getElementById("proximaP").addEventListener("click", proximaPag, false);
     document.getElementById("entrarComFace").addEventListener("click", entrarComFace, false);
 
-    entrarComFace(){
+    function entrarComFace(){
+        navigator.vibrate(200);
         facebookConnectPlugin.login(['public_profile'], function(sucesso){
-            var facebook_id = sucesso.authResponse.userID;
-            localStorage.setItem('facebook_id', facebook_id);
-
+            // var facebook_id = sucesso.authResponse.userID;
             facebookConnectPlugin.api('me', ['public_profile'], function(dados){
-                localStorage.setItem('nome', dados.name);
+                localStorage.setItem('facebook_nome', dados.name);
+                localStorage.setItem('facebook_id', dados.id);
                 document.getElementById("nomePerfil").innerHTML = dados.name;
                 document.getElementById("imagemPerfil").src = "https://graph.facebook.com/"+dados.id+"/picture/?type=large";
             })
-        }, function(erro) { alert('Erro: '+ erro); });
+        }, function(erro) { alert('Não foi possível conectar ao Facebook: '+ erro); });
     }
 
     function proximaPag() {
-        navigator.vibrate(200);
         location.href = 'pag2.html';
     }
 }
